fd3442294098d4ae1cf5ed683d692c7521355aa6
xiaojun sun <xiaojunsun@xiaojundeMacBook-Pro.local>
2022-11-22 16:24:13 +0800

    修改文件目录结构

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -15,212 +15,114 @@ module Pod
         @message_bank = MessageBank.new(self)
       end
 
-    def ask(question)
-      answer = ""
-      loop do
-        puts "\n#{question}?"
+      #运行主程序
+      def run
+        @message_bank.welcome_message
 
-        @message_bank.show_prompt
-        answer = gets.chomp
-
-        break if answer.length > 0
-
-        print "\nYou need to provide an answer."
-      end
-      answer
-    end
-
-    def ask_with_answers(question, possible_answers)
-
-      print "\n#{question}? ["
-
-      print_info = Proc.new {
-
-        possible_answers_string = possible_answers.each_with_index do |answer, i|
-           _answer = (i == 0) ? answer.underlined : answer
-           print " " + _answer
-           print(" /") if i != possible_answers.length-1
-        end
-        print " ]\n"
-      }
-      print_info.call
-
-      answer = ""
-
-      loop do
-        @message_bank.show_prompt
-        answer = gets.downcase.chomp
-
-        answer = "yes" if answer == "y"
-        answer = "no" if answer == "n"
-
-        # default to first answer
-        if answer == ""
-          answer = possible_answers[0].downcase
-          print answer.yellow
+        case pod_languge.downcase.to_sym
+          when :swift
+            ConfigureSwift.perform(configurator: self)
+          when :objc
+            ConfigureIOS.perform(configurator: self)
         end
 
-        break if possible_answers.map { |a| a.downcase }.include? answer
+        replace_variables_in_files
+        clean_template_files
+        customise_prefix
+        rename_spec_files
+        reinitialize_git_repo
+        run_pod_install
 
-        print "\nPossible answers are ["
-        print_info.call
+        @message_bank.farewell_message
       end
 
-      answer
-    end
+      #-------------------定义对象方法---------------------#
 
-    def run
-      @message_bank.welcome_message
+      #运行pod install
+      def run_pod_install
+        puts "\nRunning " + "pod install".magenta + " on your new library."
+        puts ""
 
-      platform = self.ask_with_answers("What platform do you want to use?", ["iOS", "macOS"]).to_sym
-
-      case platform
-        when :macos
-          ConfigureMacOSSwift.perform(configurator: self)
-        when :ios
-          framework = self.ask_with_answers("What language do you want to use?", ["Swift", "ObjC"]).to_sym
-          case framework
-            when :swift
-              ConfigureSwift.perform(configurator: self)
-
-            when :objc
-              ConfigureIOS.perform(configurator: self)
-          end
+        Dir.chdir("./") do
+          system "pod install"
+        end
       end
 
-      replace_variables_in_files
-      clean_template_files
-      rename_template_files
-      add_pods_to_podfile
-      customise_prefix
-      rename_classes_folder
-      ensure_carthage_compatibility
-      reinitialize_git_repo
-      run_pod_install
-
-      @message_bank.farewell_message
-    end
-
-    #----------------------------------------#
-
-    def ensure_carthage_compatibility
-      FileUtils.ln_s('Example/Pods/Pods.xcodeproj', '_Pods.xcodeproj')
-    end
-
-    def run_pod_install
-      puts "\nRunning " + "pod install".magenta + " on your new library."
-      puts ""
-
-      Dir.chdir("Example") do
-        system "pod install"
+      #删除废弃文件
+      def clean_template_files
+        ["configure", "templates", "setup", "screenshots"].each do |asset|
+          `rm -rf #{asset}`
+        end
       end
 
-      `git add Example/#{pod_name}.xcodeproj/project.pbxproj`
-      `git commit -m "Initial commit"`
-    end
+      # 替换文件中的变量内容
+      def replace_variables_in_files
+        file_names = ['LICENSE', 'README.md', 'NAME.podspec', '.travis.yml', podfile_path]
+        file_names.each do |file_name|
+          text = File.read(file_name)
+          text.gsub!("${POD_NAME}", @pod_name)
+          text.gsub!("${REPO_NAME}", @pod_name.gsub('+', '-'))
+          text.gsub!("${USER_NAME}", user_name)
+          text.gsub!("${USER_EMAIL}", user_email)
+          text.gsub!("${YEAR}", year)
+          text.gsub!("${DATE}", date)
+          File.open(file_name, "w") { |file| file.puts text }
+        end
+      end
 
-    def clean_template_files
-      ["./**/.gitkeep", "configure", "_CONFIGURE.rb", "README.md", "LICENSE", "templates", "setup", "CODE_OF_CONDUCT.md"].each do |asset|
-        `rm -rf #{asset}`
+      #自定义前缀
+      def customise_prefix
+        prefix_path = "./Tests/Tests-Prefix.pch"
+        return unless File.exists? prefix_path
+
+        pch = File.read prefix_path
+        pch.gsub!("${INCLUDED_PREFIXES}", @prefixes.join("\n  ") )
+        File.open(prefix_path, "w") { |file| file.puts pch }
+      end
+
+      #spec文件重命名
+      def rename_spec_files
+        File.rename("NAME.podspec", @pod_name + ".podspec")
+      end
+
+      #重新Git初始化
+      def reinitialize_git_repo
+        `rm -rf .git`
+        `git init`
+        `git add -A`
+      end
+
+      #验证用户信息
+      def validate_user_details
+          return (user_email.length > 0) && (user_name.length > 0)
+      end
+
+      #-------------------定义对象变量---------------------#
+
+      def user_name
+        (ENV['GIT_COMMITTER_NAME'] || github_user_name || `git config user.name` || `<GITHUB_USERNAME>` ).strip
+      end
+      
+      def github_user_name
+        github_user_name = `security find-internet-password -s github.com | grep acct | sed 's/"acct"<blob>="//g' | sed 's/"//g'`.strip
+        is_valid = github_user_name.empty? or github_user_name.include? '@'
+        return is_valid ? nil : github_user_name
+      end
+
+      def user_email
+        (ENV['GIT_COMMITTER_EMAIL'] || `git config user.email`).strip
+      end
+
+      def year
+        Time.now.year.to_s
+      end
+
+      def date
+        Time.now.strftime "%m/%d/%Y"
+      end
+
+      def podfile_path
+        './Podfile'
       end
     end
-
-    def replace_variables_in_files
-      file_names = ['POD_LICENSE', 'POD_README.md', 'NAME.podspec', '.travis.yml', podfile_path]
-      file_names.each do |file_name|
-        text = File.read(file_name)
-        text.gsub!("${POD_NAME}", @pod_name)
-        text.gsub!("${REPO_NAME}", @pod_name.gsub('+', '-'))
-        text.gsub!("${USER_NAME}", user_name)
-        text.gsub!("${USER_EMAIL}", user_email)
-        text.gsub!("${YEAR}", year)
-        text.gsub!("${DATE}", date)
-        File.open(file_name, "w") { |file| file.puts text }
-      end
-    end
-
-    def add_pod_to_podfile podname
-      @pods_for_podfile << podname
-    end
-
-    def add_pods_to_podfile
-      podfile = File.read podfile_path
-      podfile_content = @pods_for_podfile.map do |pod|
-        "pod '" + pod + "'"
-      end.join("\n    ")
-      podfile.gsub!("${INCLUDED_PODS}", podfile_content)
-      File.open(podfile_path, "w") { |file| file.puts podfile }
-    end
-
-    def add_line_to_pch line
-      @prefixes << line
-    end
-
-    def customise_prefix
-      prefix_path = "Example/Tests/Tests-Prefix.pch"
-      return unless File.exists? prefix_path
-
-      pch = File.read prefix_path
-      pch.gsub!("${INCLUDED_PREFIXES}", @prefixes.join("\n  ") )
-      File.open(prefix_path, "w") { |file| file.puts pch }
-    end
-
-    def set_test_framework(test_type, extension, folder)
-      content_path = "setup/test_examples/" + test_type + "." + extension
-      tests_path = "templates/" + folder + "/Example/Tests/Tests." + extension
-      tests = File.read tests_path
-      tests.gsub!("${TEST_EXAMPLE}", File.read(content_path) )
-      File.open(tests_path, "w") { |file| file.puts tests }
-    end
-
-    def rename_template_files
-      FileUtils.mv "POD_README.md", "README.md"
-      FileUtils.mv "POD_LICENSE", "LICENSE"
-      FileUtils.mv "NAME.podspec", "#{pod_name}.podspec"
-    end
-
-    def rename_classes_folder
-      FileUtils.mv "Pod", @pod_name
-    end
-
-    def reinitialize_git_repo
-      `rm -rf .git`
-      `git init`
-      `git add -A`
-    end
-
-    def validate_user_details
-        return (user_email.length > 0) && (user_name.length > 0)
-    end
-
-    #----------------------------------------#
-
-    def user_name
-      (ENV['GIT_COMMITTER_NAME'] || github_user_name || `git config user.name` || `<GITHUB_USERNAME>` ).strip
-    end
-
-    def github_user_name
-      github_user_name = `security find-internet-password -s github.com | grep acct | sed 's/"acct"<blob>="//g' | sed 's/"//g'`.strip
-      is_valid = github_user_name.empty? or github_user_name.include? '@'
-      return is_valid ? nil : github_user_name
-    end
-
-    def user_email
-      (ENV['GIT_COMMITTER_EMAIL'] || `git config user.email`).strip
-    end
-
-    def year
-      Time.now.year.to_s
-    end
-
-    def date
-      Time.now.strftime "%m/%d/%Y"
-    end
-
-    def podfile_path
-      'Example/Podfile'
-    end
-
-    #----------------------------------------#
   end
-end
